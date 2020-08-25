54d68d26af82fda99310ef41c0455bbbce9fbdff
MounirK <dagana.mounir@gmail.com>
2020-08-25 07:02:52 +0100

    second commit

---
+++ admin/database.php
@@ -0,0 +1,30 @@
+<?php
+    class Database{
+
+        private static $dbHost = "localhost";
+        private static $dbName = "burger_code";
+        private static $dbUser = "root";
+        private static $dbUserPassword = "";
+        private static $connection = null;
+
+        public static function connect(){
+            try{
+                self::$connection = new PDO('mysql:host='.self::$dbHost.';dbname='.self::$dbName, self::$dbUser, self::$dbUserPassword, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+                self::$connection->setAttribute ( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );   
+                self::$connection ->exec("set names utf8");
+            }catch(PDOException $e){
+                die('Error: ' . $e->getMessage());
+            }
+            return self::$connection;
+        }
+
+        public static function disconnect(){
+            self::$connection = null;
+        }
+    }
+    
+    Database::connect();
+   
+
+
+?>
